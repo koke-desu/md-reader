2093d17ff5f9122089054fc91be7d21131579766
koke-desu <gbpvp25645@yahoo.co.jp>
2022-05-31 21:07:11 +0900

    VFCが非推奨になったからFCに変更

--- components/NewStylePage/NewStylePage.tsx
+++ components/NewStylePage/NewStylePage.tsx
@@ -3,9 +3,9 @@ import React from "react";
 type ContainerProps = {};
 type Props = {} & ContainerProps;
 
-const Presenter: React.VFC<Props> = (props) => <div></div>;
+const Presenter: React.FC<Props> = (props) => <div></div>;
 
-const Container: React.VFC<ContainerProps> = (props) => {
+const Container: React.FC<ContainerProps> = (props) => {
   return <Presenter {...props} />;
 };
 
