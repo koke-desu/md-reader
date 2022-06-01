72ed0cd4afa27a12ae482c51432c8e4674348e8e
koke-desu <gbpvp25645@yahoo.co.jp>
2022-06-01 21:15:39 +0900

    クリックで開閉できるように

--- components/ExtendHeader/ExtendHeader.tsx
+++ components/ExtendHeader/ExtendHeader.tsx
@@ -1,35 +1,52 @@
 import SpecificHeader from "components/SpecificHeader";
 import { ParsedMD } from "globalState/type";
 import { isHeader } from "globalState/typeChecker";
-import React from "react";
+import React, { useState } from "react";
 import ReactMarkdown from "react-markdown";
 
 type Props = { parsedMD: ParsedMD; depth: number };
 
 const colors = ["bg-red-100", "bg-blue-100", "bg-green-100"];
 
-const ExtendHeader: React.FC<Props> = ({ parsedMD, depth }) => (
-  <>
-    {parsedMD.map((element) => {
-      if (isHeader(element)) {
-        const header = element;
-        return (
-          <div
-            key={`header-${header.title}`}
-            className={"p-4 flex mt-4 flex-col rounded-lg " + colors[depth]}
-          >
-            <SpecificHeader headLevel={depth + 1}>{header.title}</SpecificHeader>
+const ExtendHeader: React.FC<Props> = ({ parsedMD, depth }) => {
+  const [childrenOpen, setChildrenOpen] = useState<boolean[]>(Array(parsedMD.length).fill(true));
 
-            {/* 再帰してる。 */}
-            <ExtendHeader parsedMD={header.children} depth={depth + 1} />
-          </div>
-        );
-      } else {
-        const content = element;
-        return <ReactMarkdown>{content.body}</ReactMarkdown>;
-      }
-    })}
-  </>
-);
+  return (
+    <>
+      {parsedMD.map((element, index) => {
+        if (isHeader(element)) {
+          const header = element;
+          return (
+            <div
+              key={`header-${depth}-${index}`}
+              className={"p-4 flex mt-4 flex-col rounded-lg " + colors[depth]}
+              onClick={(e) => {
+                e.stopPropagation();
+                const nextOpen = childrenOpen.slice();
+                nextOpen[index] = !nextOpen[index];
+                setChildrenOpen(nextOpen);
+              }}
+            >
+              <SpecificHeader headLevel={depth + 1}>{header.title}</SpecificHeader>
+
+              {/* 子要素をレンダリングしたままにしたいから、styleを変えるだけにしてる。 */}
+              {/* childrenOpen[index] && <ExtendHeader> ってしたら子要素の、childrenOpenがリセットされちゃうからダメ。 */}
+              <div
+                className={childrenOpen[index] ? "" : "hidden"}
+                key={`content-${depth}-${index}`}
+              >
+                {/* 再帰してる */}
+                <ExtendHeader parsedMD={header.children} depth={depth + 1} />
+              </div>
+            </div>
+          );
+        } else {
+          const content = element;
+          return <ReactMarkdown>{content.body}</ReactMarkdown>;
+        }
+      })}
+    </>
+  );
+};
 
 export default ExtendHeader;
